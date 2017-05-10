d01746c1536e6f9eed97a48e1407ea6109ebd302
Thomas Samson <tsamson@advanced-schema.com>
2017-05-10 21:14:12 +0200

    starting to debug overcommit differences

--- maintenance.sh
+++ maintenance.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 
+for i in $(seq 100 20 500); do
+    docker run -m 300m python:3-alpine python -c "'a'*${i}*1024*1024" 2>/dev/null && echo -ne "\r $i" || echo "exit status for $i $?"
+done
+
+echo 2 > /proc/sys/vm/overcommit_memory
+
 for i in $(seq 100 10 500); do
-    docker run -m 300m python:3-alpine python -c "'a'*${i}*1024*1024" && echo -ne "\r $i" || echo "exit status for $i $?"
+    docker run -m 300m python:3-alpine python -c "'a'*${i}*1024*1024" 2>/dev/null && echo -ne "\r $i" || echo "exit status for $i $?"
 done
