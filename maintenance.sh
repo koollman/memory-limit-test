ca69c28c04e3e79c0a10b7245ed28b23c7ade7cf
Thomas Samson <tsamson@advanced-schema.com>
2017-05-10 21:10:07 +0200

    swap desactivation, and more useful output

--- maintenance.sh
+++ maintenance.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 
-for i in $(seq 100 10 500); do docker run -m 300m python:3-alpine python -c "print(len('a'*${i}*1024*1024)/1024/1024)" ; done
+for i in $(seq 100 10 500); do
+    docker run -m 300m python:3-alpine python -c "'a'*${i}*1024*1024" && echo -ne "\r $i" || echo "exit status for $i $?"
+done
--- provision.sh
+++ provision.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 yum update -y
+
+#docker
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum makecache fast
@@ -8,3 +10,10 @@ yum install -y docker-ce
 systemctl enable docker
 systemctl start docker
 docker pull python:3-alpine
+
+#memory limitations : too much swap by default
+swapdevice=/dev/mapper/VolGroup00-LogVol01
+swapoff $swapdevice
+lvresize -f -L 128M $swapdevice
+mkswap $swapdevice
+swapon $swapdevice
